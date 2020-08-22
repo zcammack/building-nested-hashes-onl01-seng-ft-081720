8bf60ef20ab7abadbb9837f949e8cba835fcaf82
Zackary Cammack <zack.cammack@gmail.com>
2020-08-22 16:00:15 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{name: "Bevolio", age: "17", attitude: "worried"},
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},
                         {name: "Mercutio", age: "18", attitude: "hot-headed"}
                         ]
    },
